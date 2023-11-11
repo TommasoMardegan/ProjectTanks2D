e350f608cd9a48743b9091fb4798fe5b6fd89329
TommasoMardegan <amardegan908@gmail.com>
2023-11-11 20:38:04 +0100

    Initial commit

---
+++ Server/Messaggio.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+/**
+ * classe messaggio per inviare messaggi al client
+ */
+public class Messaggio {
+    public String contenuto;
+    public String ipClient;
+    public int portClient;
+    public int serverPort = 666;
+    public String ipServer = "localhost";
+
+    Messaggio() {
+    }
+
+    Messaggio(String _ipClient, int _portClient) {
+        this.ipClient = _ipClient;
+        this.portClient = _portClient;
+    }
+
+    // invia il messaggio al Client
+    public String leggiMessaggioClient(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int bytesRead = inputStream.read(buffer);
+        String comando = new String(buffer, 0, bytesRead);
+
+        return comando;
+    }
+
+    public void inviaClient(OutputStream out, byte[] bytes) throws IOException {
+        out.write(bytes);
+    }
+
+    public PrintWriter inviaLetteraClient(Messaggio comunicazioneClient, PrintWriter writer, String lettera)
+            throws IOException {
+        String csvData = lettera;
+        writer.println(csvData);
+        return writer;
+    }
+
+    public void inviaPosizioneClient(PrintWriter writer, int posizioneXClient, int posizioneYClient) {
+        String combinedData = posizioneXClient + "," + posizioneYClient;
+        writer.println(combinedData);
+    }
+
+    public void inviaPosizioneModClient(PrintWriter writer, String messaggio) {
+        writer.println(messaggio);
+    }
+
+    public void inviaBlocchiClient(Messaggio comunicazioneClient, OutputStream outputStream, GestioneGioco gc)
+            throws IOException {
+        gestioneBlocchi gb = gc.gestioneBl;
+        int[] posXblocchi = gb.posXblocchi;
+        int[] posYblocchi = gb.posYblocchi;
+
+        byte[] posXBytes = intArrayToByteArray(posXblocchi);
+        byte[] posYBytes = intArrayToByteArray(posYblocchi);
+
+        comunicazioneClient.inviaClient(outputStream, posXBytes);
+        comunicazioneClient.inviaClient(outputStream, posYBytes);
+    }
+
+    public byte[] intArrayToByteArray(int[] arr) {
+        byte[] result = new byte[arr.length * 4];
+        for (int i = 0; i < arr.length; i++) {
+            ByteBuffer.wrap(result, i * 4, 4).putInt(arr[i]);
+        }
+        return result;
+    }
+
+    public void inviaListaCarri(PrintWriter writer, GestioneGioco gc) {
+        List<Carro> listaCarri = gc.listaCarri;
+        for(int i = 0; i < listaCarri.size(); i++) {
+            writer.println(listaCarri.get(i).letteraCarro + ";" + listaCarri.get(i).xGiocatore + ";" + listaCarri.get(i).yGiocatore);
+        }
+    }
+}
---
+++ Server/ThreadClient.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadClient implements Runnable {
+    private Messaggio comunicazioneClient;
+    private Socket clientSocket;
+    private GestioneGioco gc;
+    int[] posIniGiocatoriX;
+    int[] posIniGiocatoriY;
+    String[] lettere;
+    int indiceLettera;
+    final static int delay = 100;
+
+    public ThreadClient(Messaggio comunicazioneClient, Socket socket, GestioneGioco gc, int indiceLettera, String[] lettere, int[] posIniGiocatoriX, int[] posIniGiocatoriY) {
+        this.comunicazioneClient = comunicazioneClient;
+        this.clientSocket = socket;
+        this.indiceLettera = indiceLettera;
+        this.gc = gc;
+        this.lettere = lettere;
+        this.posIniGiocatoriX = posIniGiocatoriX;
+        this.posIniGiocatoriY = posIniGiocatoriY;
+    }
+    @Override
+        public void run() {
+            try {
+                Timer timer = new Timer();
+                InputStream inputStream = clientSocket.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                PrintWriter writer = new PrintWriter(outputStream, true);
+                boolean sincronizzato = true;
+                while (true) {
+                    //LEGGO IL MESSAGGIO RICEVUTO
+                    String comando = comunicazioneClient.leggiMessaggioClient(inputStream);
+                    System.out.println("Il server riceve: " + comando);
+
+                    //SINCRONIZZO IL CLIENT
+                    if (comando.equals("sincronizza")) {
+                        // INVIO TUTTO AL CLIENT
+                        comunicazioneClient.inviaBlocchiClient(comunicazioneClient, outputStream, gc);
+
+                        if (indiceLettera < 4) {
+                            writer = comunicazioneClient.inviaLetteraClient(comunicazioneClient, writer, lettere[indiceLettera]);
+                            comunicazioneClient.inviaPosizioneClient(writer, posIniGiocatoriX[indiceLettera], posIniGiocatoriY[indiceLettera]);
+
+                            if(indiceLettera == 0 || indiceLettera == 2) {
+                                indiceLettera++;
+                            }
+                            else {
+                                indiceLettera--;
+                            }
+
+                            writer = comunicazioneClient.inviaLetteraClient(comunicazioneClient, writer, lettere[indiceLettera]);
+                            comunicazioneClient.inviaPosizioneClient(writer, posIniGiocatoriX[indiceLettera], posIniGiocatoriY[indiceLettera]);
+                        }
+                    //CONTROLLO IL MOVIMENTO DEL CARRO
+                    } else if (comando.length() == 2) {
+                        String messaggio = gc.muoviCarro(comando.substring(0, 1), comando.substring(1, 2));
+                        //invio al client la lista di carri
+                        //prendo le posizioni a ciascuna lettera e aggiorno la posizione su grafica client
+                        //comunicazioneClient.inviaPosizioneModClient(writer, messaggio);
+                    }
+                    if(sincronizzato == true) {
+                        impostaTimer(timer, writer);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void inviaListaCarri(PrintWriter writer) {
+            comunicazioneClient.inviaListaCarri(writer, gc);
+        }
+        public void impostaTimer(Timer timer, PrintWriter writer) {
+             timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    // Implementa la logica per inviare la lista di carri
+                    inviaListaCarri(writer);
+                }
+            }, 0, 100);
+        }
+    }
+
